3e516fde468ec830f86554ad5169bc27cb2fc1a4
Niroj Tamang <niroj6988@gmail.com>
2025-12-15 18:55:27 +0545

    Allow all origins for CORS

--- index.js
+++ index.js
@@ -7,14 +7,7 @@ const app = express();
 
 // CORS middleware
 app.use((req, res, next) => {
-    const allowedOrigins = process.env.NODE_ENV === 'production' 
-        ? ['https://logistic-green-six.vercel.app', 'http://localhost:3000', 'http://127.0.0.1:3000'] 
-        : ['http://localhost:3000', 'http://127.0.0.1:3000'];
-    
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Origin', origin);
-    }
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     if (req.method === 'OPTIONS') return res.status(200).end();
