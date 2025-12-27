699b5490256350a770cda26583b8867fb78bd77f
Niroj Tamang <niroj6988@gmail.com>
2025-12-27 18:43:21 +0545

    Fix CORS for frontend domain

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // CORS middleware
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://logistic-green-six.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     if (req.method === 'OPTIONS') return res.status(200).end();
